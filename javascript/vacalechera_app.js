e389c51bdadefed5b87273f1349775f51036fe2e
Rogelio Moreno <rgl.moreno@gmail.com>
2011-10-11 19:49:27 -0500

    Ahora si añadi la clase granjero.js y limpíe la clase vacalechera_app.js

--- javascript/vacalechera_app.js
+++ javascript/vacalechera_app.js
@@ -1,44 +1,38 @@
 var loaded = false;
 var iniciado = false;
-function presionoKey(evt) {
-  if(iniciado) {
-    granjero.moverGranjero(evt);
-  }
-  else {
-    if(loaded) {
-      var texto = document.getElementById("content").getElementsByTagName("div")[0];
-      var imagen = document.getElementById("content").getElementsByTagName("img")[0];
-      var canvas = document.getElementById("fondo");
-      canvas.style.display = "block";
-      canvas.style.background = 'url(./imagenes/fondo.png)';
-      var container = document.getElementById("content");
-      container.removeChild(imagen); container.removeChild(texto);
-      vacalechera.iniciar();
-      iniciado = true;
+
+function iniciarJuego() {
+    var texto = document.getElementById("estadoLoad");
+    var imagen = document.getElementById("portada");
+    var canvas = document.getElementById("canvas0");
+    canvas.style.display = "block";
+    canvas.style.background = 'url(./imagenes/fondo.png)';
+    var container = document.getElementById("content");
+    container.removeChild(imagen); 
+    container.removeChild(texto);
+    vacalechera.iniciar();
+    iniciado = true;
+}
+
+function onKeyDown(evt) {
+    if(iniciado) {
+        granjero.moverGranjero(evt);
     }
+    else if (loaded) {
+        iniciarJuego();
+    }
+}
+
+function onClick (evt) {
+	if(iniciado) {
+        granjero.moverGranjero2(evt);
+	}
+	else if (loaded) {
+        iniciarJuego();
   }
 }
 
-function clickeo (evt) {
-	if(iniciado) {
-	granjero.moverGranjero2(evt);
-	}
-	else {
-		if(loaded) {
-		  var texto = document.getElementById("content").getElementsByTagName("div")[0];
-		  var imagen = document.getElementById("content").getElementsByTagName("img")[0];
-		  var canvas = document.getElementById("fondo");
-		  canvas.style.display = "block";
-          canvas.style.background = 'url(./imagenes/fondo.png)';
-		  var container = document.getElementById("content");
-		  container.removeChild(imagen); container.removeChild(texto);
-		  vacalechera.iniciar();
-		  iniciado = true;
-		}
-	}
-}
-
-var canvas = document.getElementById('fondo');
+var canvas = document.getElementById('canvas0');
 var ctx = canvas.getContext('2d');
 var ASSET_MANAGER = new AssetManager();
 
@@ -56,12 +50,14 @@ ASSET_MANAGER.queueDownload("./imagenes/chorro_derramado.png","chorroderramado")
 
 
 ASSET_MANAGER.downloadAll(function() {
-    document.getElementById("content").getElementsByTagName("div")[0].innerHTML = "Presiona cualquier tecla para continuar"
+    document.getElementById("estadoLoad").innerHTML = "Presiona cualquier tecla para continuar"
     vacalechera = new Juego(ctx);
     vaca = new Vaca(vacalechera, 780, 200);
     granjero = new Granjero(vacalechera, 200, 200);
-    window.addEventListener('keydown',presionoKey,true);
-	window.addEventListener('click',clickeo,true);
+    
+    window.addEventListener('keydown',onKeyDown,true);
+	window.addEventListener('click',onClick,true);
+    
     vacalechera.addEntidad(vaca);
     vacalechera.addEntidad(granjero);    
     loaded = true;
